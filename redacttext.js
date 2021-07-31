c577852f7e15ce415e9469a4075764d1619520d0
haziqzikry <haziqzikry@gmail.com>
2021-08-01 01:23:21 +0800

    init

---
+++ redacttext.js
@@ -0,0 +1,10 @@
+replaceYiddin(document.body);
+
+function replaceYiddin(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceYiddin);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    element.textContent = element.textContent.replace(/testing/gi, "████");
+    element.textContent = element.textContent.replace(/test/gi, "████");
+  }
+}
